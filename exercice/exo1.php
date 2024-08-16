6f0ace92a83969aa48ca135d8f93d76f5a465820
itachixa <xaodin12@gmail.com>
2024-08-16 10:38:28 +0000

    Add files via upload

--- exercice/exo1.php
+++ exercice/exo1.php
@@ -1,8 +1,12 @@
 <?php
-if ((int)($_GET["nom"])){
-    echo "le nom ne doit pas etre un entier";
-}elseif(isset($_GET["nom"])==null){
-    echo"Bonjour invité";
-}else{
-    echo "Bonjour ".$_GET["nom"];
+if (isset($_GET["nom"]) == null) {// Vérifie si le paramètre "nom" n'est pas défini dans l'URL
+    echo "Bonjour invité";// Si "nom" n'est pas défini, affiche un message de bienvenue pour un invité
+}elseif ((int)($_GET["nom"])) {// Vérifie si le paramètre "nom" dans l'URL est un entier 
+    echo "le nom ne doit pas etre un entier";// Si "nom" est un entier, affiche un message d'erreur
+} 
+
+else {
+    // Si "nom" est défini et n'est pas un entier, affiche un message de bienvenue personnalisé
+    echo "Bonjour " . $_GET["nom"];
 }
+?>
